c3e81d2ea14764cb87f455d42790cff44210a9fb
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-04-29 02:47:19 +0900

    옵셔널

---
+++ swiftGrammar.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+var name: String?
+
+var optionalName: String? = "HanGyeol"
+print(optionalName) //옵셔널로 감싸져 있으면 일반타입과 결합도 연산도 할 수 없음 하려면 옵셔널바인딩 필요함
+// 출력 Optional("HanGyeol")
+
+//var requiredName: String = optionalName 옵셔널이 아닌 스트링타입이기 때문에 항상 값을 가지고 있어야 함
+
