f6aae40479c15495c4156cd5b38851c400dd4b8c
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-04-28 23:44:35 +0900

    상수와 변수

--- swiftGrammar.playground/Pages/String.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var greeting = "Hello, playground"
-
-var num = 123111
-
-//: [Next](@next)
---
+++ swiftGrammar.playground/Pages/Variable Constant.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// 상수
+// let 상수명: 데이터 타입 = 값
+let a: Int = 100
+
+// 변수
+// var 변수명: 데이터 타입 = 값
+var b: Int = 200
+b = 300
