c0aa09d2c7a438213904a2647e0face72cff6788
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-04-30 11:43:44 +0900

    타입캐스팅

---
+++ swiftGrammar.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+// is 연산자로 환인 하는 방법
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "기생충", director: "봉준호"),
+    Song(name: "Butter", artist: "BTS"),
+    Movie(name: "올드보이", director: "박찬욱"),
+    Song(name: "Wonderwall", artist: "Oasis"),
+    Song(name: "Rain", artist: "이적")
+]
+
+var movieCount = 0
+var songCount = 0
+
+for item in library {
+    if item is Movie {
+        movieCount += 1
+    } else if item is Song {
+        songCount += 1
+    }
+}
+
+print("Media library contains \(movieCount) movies and \(songCount) songs")
+
+
+//형을 변환할 수 있는 다운캐스팅
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: \(movie.name), dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: \(song.name), by \(song.artist)")
+    }
+}
